2583765e2a1a9c36721376b746cca7e83632f7e0
ParaGlideRocks <michele.nappa@live.it>
2018-10-25 10:23:40 +0200

    Initial commit

---
+++ Get-ADRecipients.ps1
@@ -0,0 +1,89 @@
+#requires -version 2
+<#
+.SYNOPSIS
+  Gather msExchRecipientTypeDetails using AD cmdlet and Exchange Snapin.
+
+.DESCRIPTION
+  In order to reduce time taken to collect data we'll leverage AD cmdlets.
+
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+
+.INPUTS
+  <Inputs if any, otherwise state None>
+
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+
+.NOTES
+  Version:        1.0
+  Author:         mnappa@microsoft.com, stefcas@microsoft.com
+  Creation Date:  10/25/2018
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  C:\PS> Get-ADRecipients
+
+  
+    Description
+
+    -----------
+
+    This command collect Recipient Type Details in the specified Domain and OU.
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+
+#Dot Source required Function Libraries
+. "C:\Scripts\Functions\Logging_Functions.ps1"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = "1.0"
+
+#Log File Info
+$sLogPath = "C:\Windows\Temp"
+$sLogName = "<script_name>.log"
+$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+
+Function <FunctionName>{
+  Param()
+  
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+  
+  Process{
+    Try{
+      <code goes here>
+    }
+    
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+  
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+#>
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
+#Script Execution goes here
+#Log-Finish -LogPath $sLogFile
